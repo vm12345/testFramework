d0ad53d80550656f1dd0ec01716cb6b1f7d84dfb
MALINOVSKY Vitaly <Vitaly.MALINOVSKY@raiffeisen.ru>
2020-03-19 21:08:04 +0300

    relative paths

--- src/test/java/RunCucumberSteps.java
+++ src/test/java/RunCucumberSteps.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"/Users/ruamvvc/Documents/testFramework/src/test/resources/features"},
+        features = {"src/test/resources/features"},
         glue = {"steps",
                 "actions"})
 public class RunCucumberSteps {
